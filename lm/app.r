24f396b02a2454fd973f963904879b10063420eb
cdivoky1 <107935739+cdivoky1@users.noreply.github.com>
2022-07-12 10:08:19 -0400

    Update app.r

--- lm/app.r
+++ lm/app.r
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Shiny App: Modeling Data with Linear Regression"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
